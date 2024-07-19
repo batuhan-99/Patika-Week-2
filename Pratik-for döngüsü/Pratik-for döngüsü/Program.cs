3974d6c8797320234e3efded08f70d656b296951
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-07-20 01:15:42 +0300

    Add files via upload

---
+++ Pratik-for döngüsü/Pratik-for döngüsü/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace pratikfor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for(i=0;i<10;i++)
+                Console.WriteLine("Kendime inanıyorum ,ben bu yazılım işini yaparım");
+            for (i = 1; i < 21; i++)
+                Console.Write(i + " ");
+            Console.WriteLine();
+            for(i= 1; i < 21; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i + " ");
+            }
+            int toplam = 0;
+            for (i = 50; i < 150; i++)
+                toplam = toplam + i;
+            int toplamt = 0, toplamc = 0;
+            Console.WriteLine(toplam);
+            for (i = 1; i < 121; i++)
+            {
+                if (i % 2 == 0)
+                    toplamc = toplamc + i;
+                else
+                    toplamt = toplamt + i;
+                    
+            }
+            Console.WriteLine("Tek sayilarin toplami=" + toplamt + "\n" + "Çift sayilarin toplami=" + toplamc);
+
+        }
+    }
+}
