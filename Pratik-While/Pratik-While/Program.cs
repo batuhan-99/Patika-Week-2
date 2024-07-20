3a0fce75da5ae2438234e9b052ad3ef87b89b285
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-07-20 11:28:21 +0300

    Add files via upload

---
+++ Pratik-While/Pratik-While/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PratikWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini yaparım.");
+                i++;
+            }
+            i = 1;
+            while (i < 21)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            Console.WriteLine();
+            i = 1;
+            while(i < 21)
+            {
+                if (i % 2 == 0)
+                    Console.Write(i + " ");
+                i++;
+            }
+            Console.WriteLine();
+            i = 50;
+            int toplam = 0;
+            while (i <= 150)
+            {
+                toplam = toplam + i;
+                i++;
+            }
+            Console.WriteLine("Toplam = " + toplam);
+            i = 1;
+            int toplamt = 0, toplamc = 0;
+            while(i < 121)
+            {
+                if (i % 2 == 0)
+                    toplamc = toplamc + i;
+                else
+                    toplamt = toplamt + i;
+                i++;
+            }
+            Console.WriteLine("Tek sayıların toplam=" + toplamt + "\n" + "Çift sayıların toplamı=" + toplamc);
+
+        }
+    }
+}
