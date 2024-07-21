ffa08c9d75ceb16b4ada0d24ddf81306b9dd3893
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-07-21 13:50:32 +0300

    Add files via upload

---
+++ Pratik Do-While vs While/Pratik Do-While vs While/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            Console.WriteLine("Limit belirleyin.");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            limit = Math.Abs(limit);
+            /*  while (sayac < limit)
+              {
+                  Console.WriteLine("Ben bir patikalıyım.");
+                  sayac++;
+              }*/
+            do
+            {
+                Console.WriteLine("Ben bir patikalıyım.");
+                sayac++;
+            } while (sayac < limit);
+        }//Do-while bazı durumlarda while dan daha kullanışlıdır.Çünkü kod en az bir kere çalıştırılacağı için while'daki gibi önceden fazladan
+         //bir kod yazma zorunluluğu yoktur
+    }
+}
